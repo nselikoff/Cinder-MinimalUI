5e37eb879f5d6b0b2225cd8bb6d57daa4839362f
Nathan Selikoff <nselikoff@gmail.com>
2014-01-13 14:32:51 -0500

    fix a few more spaces to tabs

--- include/UIElement.h
+++ include/UIElement.h
@@ -27,36 +27,36 @@ namespace MinimalUI {
 		
 		UIController* getParent() const { return mParent; }
 
-        ci::Vec2i getPosition() const { return mPosition; }
-        
-        ci::Vec2i getSize() const { return mSize; }
-        void setSize( const ci::Vec2i &aSize ) { mSize = aSize; }
-        
-        ci::Area getBounds() const { return mBounds; }
-        
-        bool hasParam( const std::string &aName ) const { return mParams.hasChild( aName ); }
-        template <class T>
-        T getParam( const std::string &aName ) const { return mParams[aName].getValue<T>(); }
-        
-        bool isActive() const { return mActive; }
-        void setActive( const bool &aActive ) { mActive = aActive; }
-        void deactivate() { mActive = false; }
-        bool isLocked() const { return mLocked; }
-        
-        void setBackgroundTexture( const ci::gl::Texture &aBackgroundTexture ) { mBackgroundTexture = aBackgroundTexture; }
-        
-        ci::ColorA getBackgroundColor() const { return mBackgroundColor; }
-        void setBackgroundColor( const ci::ColorA &aBackgroundColor ) { mBackgroundColor = aBackgroundColor; }
-        
-        ci::ColorA getForegroundColor() const { return mForegroundColor; }
-        void setForegroundColor( const ci::ColorA &aForegroundColor ) { mForegroundColor = aForegroundColor; }
+		ci::Vec2i getPosition() const { return mPosition; }
+		
+		ci::Vec2i getSize() const { return mSize; }
+		void setSize( const ci::Vec2i &aSize ) { mSize = aSize; }
+		
+		ci::Area getBounds() const { return mBounds; }
+		
+		bool hasParam( const std::string &aName ) const { return mParams.hasChild( aName ); }
+		template <class T>
+		T getParam( const std::string &aName ) const { return mParams[aName].getValue<T>(); }
+		
+		bool isActive() const { return mActive; }
+		void setActive( const bool &aActive ) { mActive = aActive; }
+		void deactivate() { mActive = false; }
+		bool isLocked() const { return mLocked; }
+		
+		void setBackgroundTexture( const ci::gl::Texture &aBackgroundTexture ) { mBackgroundTexture = aBackgroundTexture; }
+		
+		ci::ColorA getBackgroundColor() const { return mBackgroundColor; }
+		void setBackgroundColor( const ci::ColorA &aBackgroundColor ) { mBackgroundColor = aBackgroundColor; }
+		
+		ci::ColorA getForegroundColor() const { return mForegroundColor; }
+		void setForegroundColor( const ci::ColorA &aForegroundColor ) { mForegroundColor = aForegroundColor; }
 
 		ci::ColorA getNameColor() const { return mNameColor; }
 		void setNameColor( const ci::ColorA &aNameColor ) { mNameColor = aNameColor; renderNameTexture(); }	  
 
 		std::string getName() const { return mName; }
 		void setName( const std::string &aName ) { mName = aName; renderNameTexture(); }
- 
+
 		ci::gl::Texture getNameTexture() const { return mNameTexture; }
 		void setNameTexture( const ci::gl::Texture &aTexture ) { mNameTexture = aTexture; }
 		void drawLabel();
@@ -82,22 +82,22 @@ namespace MinimalUI {
 		
 		ci::signals::scoped_connection mCbMouseDown, mCbMouseUp, mCbMouseDrag;
 
-        UIController *mParent;
-        ci::Vec2i mPosition;
-        ci::Vec2i mSize;
-        ci::Area mBounds;
-        ci::JsonTree mParams;
-        std::string mName;
-        std::string mGroup;
-        ci::Font mFont;
-        ci::TextBox::Alignment mAlignment;
-        ci::gl::Texture mBackgroundTexture;
-        ci::gl::Texture mNameTexture;
-        ci::ColorA mBackgroundColor, mForegroundColor, mNameColor;
-        bool mActive;
-        bool mLocked;
-        bool mIcon;
-        bool mClear;
+		UIController *mParent;
+		ci::Vec2i mPosition;
+		ci::Vec2i mSize;
+		ci::Area mBounds;
+		ci::JsonTree mParams;
+		std::string mName;
+		std::string mGroup;
+		ci::Font mFont;
+		ci::TextBox::Alignment mAlignment;
+		ci::gl::Texture mBackgroundTexture;
+		ci::gl::Texture mNameTexture;
+		ci::ColorA mBackgroundColor, mForegroundColor, mNameColor;
+		bool mActive;
+		bool mLocked;
+		bool mIcon;
+		bool mClear;
 
 	};
 	
